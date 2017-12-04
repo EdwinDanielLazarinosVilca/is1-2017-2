6c1ab1f2be8e90ec801862cd55e9596b87593ca4
Edwin Daniel Lazarinos Vilca <edwin.lazarinos@ucsp.edu.pe>
2017-12-03 20:35:57 -0500

    Add_Alumno_SErvice

---
+++ src/main/java/service/AlumnoService.java
@@ -0,0 +1,39 @@
+package service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import domain.Alumno;
+import repository.AlumnoRepository;
+
+@Service
+public class AlumnoService {
+
+  @Autowired
+  AlumnoRepository repository;
+
+  public List<Alumno> getAll() {
+    return repository.findAll();
+  }
+
+  public Alumno getByCodigo(String codigo) {
+    return repository.findOne(codigo);
+  }
+  public Alumno getByAlumno(String ApellidoPateterno) {
+	    return repository.findAlumnoName(ApellidoPateterno);
+	  }
+
+  public Alumno save(Alumno alumnoChanged) {
+    System.out.println("Modificando: " + alumnoChanged.getId());
+    Alumno alumno = repository.findOne(alumnoChanged.getNombres());
+    if (alumno != null) {
+      alumno.setApellidoPaterno(alumnoChanged.getApellidoPaterno());
+      alumno.setApellidoMaterno(alumnoChanged.getApellidoMaterno());
+      alumno.setNombres(alumnoChanged.getNombres());
+    } else {
+      alumno = alumnoChanged;
+    }
+
+    return repository.save(alumno);
+  }
+}
